c258ecedaa9f5f02fca3220d56d2f86c94e4310d
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-01-22 10:56:24 +0000

    n

--- lec_6_x.dop_task_2.py
+++ lec_6_x.dop_task_2.py
@@ -1,12 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-def ellipse(a=10,b=5,p=2,e=0.9):
- phi=np.arange(0.8*np.pi,0.01)
- r=p/(1+e*np.cos(phi))
- x=r*np.cos(phi)
- y=r*np.sin(phi)
+def ellipse(a=8,b=4,p=2,e=0.8):
+ ph=np.arange(0,7*np.pi,0.01)
+ r=p/(1+e*np.cos(ph))
+ x=r*np.cos(ph)
+ y=r*np.sin(ph)
  plt.plot(x,y)
  plt.show()
 ellipse()
-
-
---
+++ lec_6_x.dop_task_4.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.linspace(0, 10, 1000)
+y = []
+for i in range(1000):
+    y1 = x[i] // 1
+    y.append(y1)
+plt.plot(x, y)
+plt.show()
