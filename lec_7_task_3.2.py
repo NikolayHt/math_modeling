e493c837b9a2df048ca03b77c996ed13f8a77bda
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-02-08 18:57:09 +0000

    w

--- lec_7_task_3.2.py
+++ lec_7_task_3.2.py
@@ -1,29 +1,30 @@
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 fig, ax = plt.subplots()
 heart, = plt.plot([], [], '-', color='r', label='heart')
+xdata, ydata = [], [] 
+ax.set_xlim(-20,20)
+ax.set_ylim(-20,20)
 
-def loveis(time):
-    alpha = (0,2*np.pi,0.1)
-    x = 16*np.sin(alpha)**3
-    y = 13*np.cos(alpha)-5*np.cos(2*alpha)-2*np.cos(3*alpha)-np.cos(4*alpha)
-    return x, y
+def update(time):
+    t = 0.1*time
+    x = 16*np.sin(t)**3 
+    y = 13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t)
+    return x,y 
+    
+    xdata.append(x)
+    ydata.append(y)
+    heart.set_data(xdata,ydata)
+    return heart,
 
+ani = FuncAnimation(fig, # Стандартный вызов пространства для анимации
+                    update, # Вызов функции подстановки координат
+                    frames=100,
+                    interval=30 # Интервал между кадрами,
+                    )            # по умолчанию 200 милисекунд
 
-plt.axis('equal')
-ax.set_xlim(-20, 20)
-ax.set_ylim(-20, 20)
-
-def animate(i):
-    heart.set_data(loveis(time=i))
-
-ani = animation.FuncAnimation(fig,
-                              animate,
-                              frames=100,
-                              interval=30
-                              )
 
 plt.show()
 
