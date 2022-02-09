588c33dc38cecfd9ac3612882f1b511b41b53c0d
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-02-09 18:49:53 +0000

    what

--- lec_7_task_3.1a.py
+++ lec_7_task_3.1a.py
@@ -4,25 +4,24 @@ import numpy as np
 
 fig, ax = plt.subplots()
 fly, = plt.plot([], [], '-', color='y', label='fly')
+xdata, ydata = [], []
 
-def babochka(time):
-    alpha = np.arange(0,12*np.pi,0.1) 
-    x = np.sin(alpha)*(np.exp(np.cos(alpha))-2*np.cos(4*alpha)+np.sin(alpha/12)**5)
-    y = np.cos(alpha)*(np.exp(np.cos(alpha))-2*np.cos(4*alpha)+np.sin(alpha/12)**5)
-    return x, y
-
-
-plt.axis('equal')
 ax.set_xlim(-5, 5)
 ax.set_ylim(-5, 5)
 
-def animate(i):
-    fly.set_data(babochka(time=i))
+def babochka(time):
+    alpha = np.arange(0,12*np.pi,0.1) 
+    xdata = np.sin(alpha)*(np.exp(np.cos(alpha))-2*np.cos(4*alpha)+np.sin(alpha/12)**5)
+    ydata = np.cos(alpha)*(np.exp(np.cos(alpha))-2*np.cos(4*alpha)+np.sin(alpha/12)**5)
+    anim_object.set_data(xdata, ydata)
+    return fly,
 
 ani = animation.FuncAnimation(fig,
-                              animate,
+                              fly,
                               frames=360,
                               interval=30
                               )
 
 plt.show()
+
+
---
+++ lec_7_task_4.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
+
+fig,ax=plt.subplots()
+frakt,=plt.plot([],[],'o',color='g',label='Frakt')
+
+ax.set_xlim(-0.2,0.4)
+ax.set_ylim(0,0.8)
+x,y=[],[]
+
+def fr(n=100,x0=0.1,y0=0.1):
+  x.append(x0)
+  y.append(y0)
+  C=0.3
+  D=0.33
+  for i in range(n):
+    xL=x[-1]
+    yL=y[-1]
+    x.append(xL**2-yL**2+C)
+    y.append(2*xL*yL+D)
+
+xdata,ydata=[],[]
+
+def update(i):
+  xdata.append(x[i])
+  ydata.append(y[i])
+  frakt.set_data(xdata,ydata)
+  return frakt,
+fr()
+
+ani=FuncAnimation(fig,
+                  update,
+                  frames=100,
+                  interval=100
+                  )
+
+plt.show()
