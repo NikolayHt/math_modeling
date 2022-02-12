cc9a7b6bcf44290d396c7af354e966460347d623
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-02-12 12:52:38 +0000

    w

--- lec_7_task_3.2.py
+++ lec_7_task_3.2.py
@@ -12,18 +12,17 @@ def update(time):
     t = 0.1*time
     x = 16*np.sin(t)**3 
     y = 13*np.cos(t)-5*np.cos(2*t)-2*np.cos(3*t)-np.cos(4*t)
-   
     
     xdata.append(x)
     ydata.append(y)
     heart.set_data(xdata,ydata)
     return heart,
 
-ani = FuncAnimation(fig, # Стандартный вызов пространства для анимации
-                    update, # Вызов функции подстановки координат
+ani = FuncAnimation(fig, 
+                    update, 
                     frames=100,
-                    interval=30 # Интервал между кадрами,
-                    )            # по умолчанию 200 милисекунд
+                    interval=30 
+                    )            
 
 
 plt.show()
