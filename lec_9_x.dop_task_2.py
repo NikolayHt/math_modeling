5a73411ae7a49698952d42a8cd79baa95f783e42
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-02-24 16:04:34 +0000

    n

--- lec_9_x.dop_task_2.py
+++ lec_9_x.dop_task_2.py
@@ -6,7 +6,7 @@ t = np.arange(0, 24, 0.1)
 
 
 def radio_function(S, t):
-  dsdt = 2*r*np.sqrt(np.pi*S*r*S*np.cos(np.pi-(t-12)/t))
+  dsdt = 2 * r * np.sqrt (np.pi * S * r * S * np.cos (np.pi - (t - 12) / t))
   return dsdt
 S_0 = 1600
 r = 1365,4
