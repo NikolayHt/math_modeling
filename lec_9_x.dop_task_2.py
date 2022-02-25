70bedb6a94dba238f68428e9fe3e02a802bb726f
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-02-25 15:37:57 +0000

    n

--- lec_9_x.dop_task_2.py
+++ lec_9_x.dop_task_2.py
@@ -5,10 +5,11 @@ import matplotlib.pyplot as plt
 t = np.arange(1, 24, 0.1)
 
 def radio_function(S, t):
-  dsdt = 2 * r * np.sqrt (np.pi * S * r * S * np.cos (np.pi - (t - 12) / t))
+  dsdt = k * np.sqrt(S / np.pi) * E * S * np.cos(np.pi / 12 * (t - 12))       
   return dsdt
-S_0 = 1600
-r = 1365,4
+S_0 = 16
+E = 1365.4
+k = 340 * 10 ** (-8)
 solve= odeint(radio_function, S_0, t)
 
 plt.plot(t, solve[:, 0])
