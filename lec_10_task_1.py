7b8b1877d397ef1b3ff74ed4360ccbd9156d2d5b
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-02-25 16:18:07 +0000

    b

--- lec_10_task_1.py
+++ lec_10_task_1.py
@@ -16,6 +16,7 @@ k0 = y0, z0
 
 sol = odeint(diff_func, k0, x)
 plt.plot(x, sol[:, 1], 'g', label = "y(x)")
+plt.plot(x, sol[:, 0], 'y', label = "z(x)")        
 plt.legend()
 plt.show()
 
---
+++ lec_10_task_2.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
+t = np.arange (-1, 1, 0.01)
+
+
+def diff_func(k, t):
+  x, y = k
+  dxdt = 3 * x - 2 * y + (np.e**(3*t)/np.e**t + 1)
+  dydt = x - (np.e**(3*t)/np.e**t + 1)
+  return dxdt, dydt
+
+x0 = 5
+y0 = -7
+k0 = x0, y0
+
+sol = odeint(diff_func, k0, t)
+plt.plot(t, sol[:, 1], 'g', label = "x(t)")
+plt.plot(t, sol[:, 0], 'y', label = "y(t)")        
+plt.legend()
+plt.show()
+
+
+
