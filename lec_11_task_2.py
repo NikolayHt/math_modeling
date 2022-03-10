3bd6d5f7ea8016dade0acadaf07639db490fb0f8
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-03-10 17:19:27 +0000

    n

--- lec_11_task_2.py
+++ lec_11_task_2.py
@@ -1,27 +1,31 @@
+
+
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 frames = 200
-t = np.linspace(0, 5, frames)
+t = np.linspace(0, 50, frames) 
 
-def move_func(z, t):
+def move_function(z, t):
   x, y = z
-  dxdt = u_x
-  dydt = u_y
+  dxdt = (m - x - y) * k1
+  dydt = (m - x - y) * k2
   return dxdt, dydt
 
-g = 9.8
-u = 20
-k = 2
-m = 0.5
-alf = 50 * np.pi / 180
+u = 15
+m = 10
+k1 = 0.2
+k2 = 0.5
 x0 = 0
-u_x0 = u * np.cos(alf)
 y0 = 0
-u_y0 = u * np.sin(alf)
-z0 = x0, u_x0, y0, u_y0
 
+z0 = x0, y0
 
+sol = odeint(move_function, z0, t)
 
+plt.plot(t, sol[:, 1], "r", label="y(x)")
+plt.plot(t, sol[:, 0], "b", label="z(x)")
+plt.legend()
 plt.show()
