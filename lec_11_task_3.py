9c3631db7bed2cae5ef9c778a59eaddfd3f5e2af
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2022-03-11 16:15:46 +0000

    b

--- lec_11_task_3.py
+++ lec_11_task_3.py
@@ -4,27 +4,34 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 frames = 100
-t = np.linspace(0, 5, frames) 
+t = np.linspace(0, 50, frames) 
 
 def move_function(z, t):
-  y, u_y = z
+  x, u_x, y, u_y = z
+  dxdt = x0
+  du_xdt =u_x0
   dydt = u_y
-  du_ydt = (y/(8*m)) - g
-  return dydt, du_ydt
+  du_ydt = (y/(l*m)) - g
+  return dxdt, du_xdt, dydt, du_ydt
 
 g = 9.8
+u = 0.5
 m = 0.5
-
-y0 = -0.08
-u_y0 = 0.5
-
-z0 = y0, u_y0
+l = -0.08
+y0 = l
+u_y0 = u
+x0 = 0
+u_x0 = 0
+z0 = x0, u_x0, y0, u_y0
 
 def solve_function(i, key):
   sol = odeint(move_function, z0, t)
   if key == 'point':
     x = sol[i, 0]
     y = sol[i, 2]
+  else:
+    x = sol[:i, 0]
+    y = sol[:i, 2]
   return x, y
 
 fig, ax = plt.subplots()
@@ -38,8 +45,8 @@ ani = FuncAnimation(fig,
                     frames=frames,
                     interval=30)
 
-edge = 30
-ax.set_xlim(0, edge)
-ax.set_ylim(0, edge)
+edge = 0.8
+ax.set_xlim(-edge, edge)
+ax.set_ylim(-edge, edge)
 
 plt.show()
