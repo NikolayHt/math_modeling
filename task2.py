86f2393907a4850dba761a0f08f4188f2ae55072
NikolayHt <95091513+NikolayHt@users.noreply.github.com>
2023-01-26 16:01:28 +0000

    s

---
+++ task2.py
@@ -0,0 +1,30 @@
+s = int(input('Введите число: '))
+if s >= 0 or s <= 100:
+  print('Число корректно')
+  print('Начинаю угадывать Ваше число:')  
+  ge = (x+1 for x in range (100))
+  a = 50
+  print('Ваше число меньше 50: ')
+  
+
+
+
+left, right = 0, len(array)
+while left < right:
+	middle = (left + right) / 2
+  if array[middle] < target
+  	left = middle + 1
+  else
+  	right = middle
+return left
+  
+else:
+  print('Число НЕкорректно')
+  break
+print('Начинаю угадывать Ваше число:')  
+
+
+
+
+
+
